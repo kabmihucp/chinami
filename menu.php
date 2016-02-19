d990603be4083690d73d6feeb4787896cf5c32e5
kabmihucp <kabmihucp@yahoo.co.jp>
2016-02-19 21:48:46 +0900

    test
    bakaguchi

---
+++ menu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link rel="stylesheet" type="text/css" href="cats.css" media="all">
+<title>CATS</title>
+</head>
+<body>
+<p class="t_login">CATS</p>
+<div id="content">
+<form action="login_db.php">
+	<table>
+		<tr>
+			<td><p class="m_login">Login ID </p></td>
+			<td><p class="m_login">:</p></td>
+			<td><input type="text" name="login_id"></td>
+		</tr>
+		<tr>
+			<td><p class="m_login">Password </p></td>
+			<td><p class="m_login">:</p></td>
+			<td><input type="password" name="login_pass"></td>
+		</tr>
+	</table>
+	<input type="submit" value="LOGIN" class="login">
+</form>
+</div>
+</body>
+</html>
